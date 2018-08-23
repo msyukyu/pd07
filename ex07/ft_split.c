1ff359c6ed32efeaac8c3130861c439a176af418
Damien Abeloos <dabeloos@w-r3-p3.s19.be>
2018-08-23 20:56:58 +0200

    ft_split

--- ex07/ft_split.c
+++ ex07/ft_split.c
@@ -5,108 +5,145 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/08/23 10:00:34 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/23 14:38:34 by dabeloos         ###   ########.fr       */
+/*   Created: 2018/08/23 17:59:42 by dabeloos          #+#    #+#             */
+/*   Updated: 2018/08/23 20:56:35 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-int		ft_count_elems(char *str)
+char	*ft_strstr(char *str, char *to_find)
+{
+	int		i;
+
+	if (to_find[0] == '\0')
+		return (str);
+	while (*str != '\0')
+	{
+		while (*str != *to_find && *str != '\0')
+			str++;
+		if (*str == *to_find)
+		{
+			i = 0;
+			while (str[i] != '\0' || (str[i] == '\0' && to_find[i] == '\0'))
+			{
+				if (to_find[i] == '\0')
+					return (str);
+				if (to_find[i] == str[i])
+					i++;
+				else
+					break ;
+			}
+			str++;
+		}
+	}
+	return ((void*)0);
+}
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = -1;
+	while (++i < n && src[i] != '\0')
+		dest[i] = src[i];
+	while (i++ < n)
+		dest[i - 1] = '\0';
+	return (dest);
+}
+
+int		ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+int		ft_count_words(char *str, char *charset)
 {
 	int	n;
 	int	i;
+	int	j;
 	int	check;
 
 	n = 0;
 	i = 0;
 	check = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] != ' ' && str[i] != '\t' && str[i] != '\n' && !check)
-			check++;
-		else if (check)
-		{
-			n++;
-			check--;
-		}
-		i++;
-	}
-	if (check)
-		n++;
-	return (n);
-}
-
-void	ft_init_var(int var[3], int zero)
-{
-	var[0] = (zero) ? 0 : -1;
-	var[1] = 0;
-	var[2] = 0;
-}
-
-char	*ft_replace_by_space(char *str, char *charset)
-{
-	int		v[3];
-	char	*output;
-
-	ft_init_var(v, 1);
-	while (str[v[0]++] != '\0')
-		v[1]++;
-	output = malloc(sizeof(char) * (v[1] + 1));
-	output[v[1]] = '\0';
-	ft_init_var(v, 0);
-	while (str[++v[0]] != '\0')
-	{
-		output[v[0]] = str[v[0]];
-		if (v[1] == 0 && str[v[0]] == charset[v[1]])
-		{
-			v[2] = v[0];
-			while (str[v[0] + 1] != '\0' && str[++v[0]] == charset[v[1] + 1])
-				v[1]++;
-			if (charset[v[1] + 1] == '\0')
-				while (++v[2] < v[0] + 1)
-					output[v[2] - 1] = ' ';
-			v[1] = 0;
-			v[0]--;
-		}
-	}
-	return (output);
-}
-
-char	**ft_sub_split(size_t l, char **result, int var[3], char *copy)
-{
 	while (1)
 	{
-		if (copy[var[1]] != ' ' && copy[var[1]] != '\t' &&
-				copy[var[1]] != '\n' && copy[var[1]] != '\0')
-			l += sizeof(char);
-		else if (l > 0)
+		j = 0;
+		while (str[i + j] == charset[j] && charset[j] != '\0')
+			j++;
+		if (charset[j] == '\0' || (str[i] == '\0' && j == 0))
 		{
-			result[var[0]] = malloc(l + 1);
-			var[2] = 0;
-			var[1] = var[1] - l;
-			while (var[2] < (int)(l / sizeof(char)))
-				result[var[0]][var[2]++] = copy[var[1]++];
-			result[var[0]++][var[2]] = '\0';
-			l = 0;
+			if (check && i > 0 && !(charset[j] == '\0' && str[i + j] == '\0'))
+			{
+				check = 0;
+				n++;
+			}
+			if (str[i] != '\0')
+				i = i + j - 1;
 		}
-		if (copy[var[1]++] == '\0')
+		else
+			check = 1;
+		if (str[i] == '\0')
 			break ;
+		i++;
 	}
-	result[var[0]] = NULL;
-	return (result);
+	return (n);
 }
 
 char	**ft_split(char *str, char *charset)
 {
-	size_t	l;
+	char	*dest;
+	char	*trash;
+	int		length;
 	char	**result;
-	int		var[3];
-	char	*copy;
+	int		i;
+	int		count;
 
-	copy = ft_replace_by_space(str, charset);
-	result = malloc(sizeof(char*) * (ft_count_elems(copy) + 1));
-	ft_init_var(var, 1);
-	l = 0;
-	return (ft_sub_split(l, result, var, copy));
+	count = ft_count_words(str, charset) + 1;
+	result = malloc(sizeof(char*) * (count));
+	i = 0;
+	while (i < count)
+	{
+		dest = malloc(sizeof(char) * (1 + ft_strlen(str)));
+		dest = ft_strncpy(dest, str, 1 + ft_strlen(str));
+		trash = ft_strstr(dest, charset);
+		length = trash - dest;
+		if (length > 0)
+		{
+			(*result)[i] = malloc(length + 1);
+			(*result)[i] = strncpy((*result)[i], dest, length);
+			(*result)[i][length] = '\0';
+			dest += length;
+		}
+		i++;
+		dest++;
+	}
+	(*result)[i] = NULL;
+	return (*result);
+}
+#include <stdio.h>
+int		main(int argc, char *argv[])
+{
+	char **result;
+	int i;
+
+	if (argc == 3)
+	{
+		result = ft_split(argv[1], argv[2]);
+		i = 0;
+		while ((*result)[i] != NULL)
+		{
+			printf("%d", (*result)[i]);
+			i++;
+		}
+	}
+	return (0);
 }
