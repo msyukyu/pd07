2482ee4300f07f85270723a16473ed54ed2cdc8b
Damien Abeloos <dabeloos@w-r3-p3.s19.be>
2018-08-23 15:25:11 +0200

    ft_print_words_tables

--- ex05/ft_print_words_tables.c
+++ ex05/ft_print_words_tables.c
@@ -6,7 +6,7 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/21 18:32:06 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/21 18:42:04 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/23 15:24:58 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ void	ft_print_words_table(char **tab)
 	int	j;
 
 	i = 0;
-	while (tab[i] != NULL)
+	while (tab[i] != (void*)0)
 	{
 		j = 0;
 		while (tab[i][j] != '\0')
