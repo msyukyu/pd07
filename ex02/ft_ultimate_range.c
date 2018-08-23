32ff5a32f81f828e5283775c25265358da4aa338
Damien Abeloos <dabeloos@w-r3-p3.s19.be>
2018-08-23 14:24:45 +0200

    ft_ultimate

--- ex02/ft_ultimate_range.c
+++ ex02/ft_ultimate_range.c
@@ -6,21 +6,20 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/21 08:55:26 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/22 14:53:32 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/23 14:24:35 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
-#include <stdio.h>
+
 int		ft_ultimate_range(int **range, int min, int max)
 {
 	int	i;
 
 	if (min >= max)
 		return (0);
-	range = malloc(sizeof(int*));
 	*range = malloc(sizeof(int) * ((long)max - (long)min));
-	if (range == NULL)
+	if (*range == NULL)
 		return (0);
 	i = 0;
 	while (i < (long)max - (long)min)
